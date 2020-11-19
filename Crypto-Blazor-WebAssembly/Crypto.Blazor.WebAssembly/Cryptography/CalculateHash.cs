723f8666834953d2edbe5c7e3084406f2469434a
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-11-19 00:13:50 -0300

    Create CalculateHash static method

---
+++ Crypto-Blazor-WebAssembly/Crypto.Blazor.WebAssembly/Cryptography/CalculateHash.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Crypto.Blazor.WebAssembly.Cryptography
+{
+    public class CalculateHash
+    {
+        public static string SHA256HashFunction (string plainText)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // Compute Hash
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(plainText));
+
+                // Convert byte array to string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+    }
+}
